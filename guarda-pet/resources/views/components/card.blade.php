fba65e985778ba4a6341911b942c39e6a48a822a
Adriel Melo <adrielpakolino@gmail.com>
2025-03-17 15:29:04 -0300

    feature: criando componente para card de animais

---
+++ guarda-pet/resources/views/components/card.blade.php
@@ -0,0 +1,9 @@
+<div class="w-[290px] h-[515px] border-2 border-[#0098DA] rounded-3xl overflow-hidden relative flex items-end">
+    <div class="h-[220px] overflow-hidden absolute top-0 z-0">
+        <img src="{{ asset('img/cat.png') }}" alt="gato">
+    </div>
+    <div class="w-[290px] h-[330px] rounded-t-3xl bg-white z-1">
+        <span>{{ $nome }}</span>
+    </div>
+    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
+</div>
