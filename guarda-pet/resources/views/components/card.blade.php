47d4110136bc9b86ec59b62a7e78fac7d40ca2a1
Adriel Melo <adrielpakolino@gmail.com>
2025-03-18 09:47:25 -0300

    refactor: refazendo card dos animais (estava bugando provavelmente chace mas acabei refazendo)

--- guarda-pet/resources/views/components/card.blade.php
+++ guarda-pet/resources/views/components/card.blade.php
@@ -2,8 +2,26 @@
     <div class="h-[220px] overflow-hidden absolute top-0 z-0">
         <img src="{{ asset('img/cat.png') }}" alt="gato">
     </div>
-    <div class="w-[290px] h-[330px] rounded-t-3xl bg-white z-1">
-        <span>{{ $nome }}</span>
+    <div class="w-[290px] h-[330px] rounded-t-3xl bg-white z-1 p-6 flex flex-col ">
+        <div class="flex justify-between mb-4">
+            <div class="flex flex-col">
+                <span class="text-[#0098DA] font-bold text-xl">{{ $nome }}</span>
+                <p class="text-xs text-black/60">{{ $sexo }} * {{ $idade }} * {{ $porte }}</p>
+            </div>
+            <div class="w-8 h-8 bg-white border-[#0098DA] border-1 rounded-full flex items-center justify-center">
+                <i class="fa-solid fa-heart text-base text-[#0098DA]"></i>
+            </div>
+        </div>
+        <div class="mb-2">
+            <p class="text-[11px] text-black/80">
+                {{ $descricao }}
+            </p>
+        </div>
+        <div class="w-[240px] h-[40px] bg-[#F58634] rounded-sm flex items-center justify-center">
+            <span class="text-base font-medium text-white">Em processo de adoção
+            </span>
+        </div>
+        <img class="w-[68px] -top-1 -right-0 absolute" src="{{ asset('img/pet.png') }}" alt="">
     </div>
     <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
 </div>
